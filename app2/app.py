acebce7b8f546dd47940c438f82a726a62c81cff
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2024-07-31 11:19:08 +0530

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Welcome to CSM Technology Google Kubernetes Engine Platform!'
+    return 'Welcome to CSM Technology Google Kubernetes Engine Platform Using by Public GitHub Action!'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
